08ed6ee7e05df0eb501d013282363b58aefff9be
Lívia Della Garza <liviadgarza@gmail.com>
2025-10-24 11:32:04 -0300

    Add: tela de livros

--- code/biblioteca/views.py
+++ code/biblioteca/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.db.models import Count, Q
+from django.http import HttpResponse
+from django.template import loader
+from .models import Livro
+from .models import Exemplar
 
-# Create your views here.
+def livros(request):
+    livros = Livro.objects.annotate(
+        exemplares_disponiveis=Count(
+            'exemplar',
+            filter=Q(exemplar__status=Exemplar.StatusType.DISPONIVEL)
+        )
+    )
+    context = {
+        'livros': livros
+    }
+    template = loader.get_template('livros.html')
+    return HttpResponse(template.render(context, request))
