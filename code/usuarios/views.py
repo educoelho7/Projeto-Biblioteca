4a295b1b7bd51290e4a2c00b803a6abe51b044f9
Lívia Della Garza <liviadgarza@gmail.com>
2025-11-01 12:53:41 -0300

    Add: login e logout

--- code/usuarios/views.py
+++ code/usuarios/views.py
@@ -1,10 +1,24 @@
 from .models import Usuario
 from django.contrib import messages
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as login_django
+from django.contrib.auth import logout as logout_django
+from django.shortcuts import redirect
 
 def login(request):
-    return render(request, 'login.html')
+    if request.method == "GET":
+        return render(request, 'login.html')
+    else:
+        usuario = request.POST.get('usuario')
+        senha = request.POST.get('senha')
+        user = authenticate(username=usuario, password=senha)
+        if user:
+            login_django(request, user)
+            return redirect('livros')
+        else:
+            messages.error(request, 'Usuário ou senha inválidos. Tente novamente.')
+            return render(request, 'login.html')
 
 def cadastro(request):
     if request.method == "GET":
@@ -32,3 +46,9 @@ def cadastro(request):
         user.save()
         
         return render(request, 'login.html')
+    
+def logout(request):
+    logout_django(request)
+    return render(request, 'login.html')
+
+
